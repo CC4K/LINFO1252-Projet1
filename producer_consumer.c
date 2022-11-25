d81c00af2a088ed94a8be52426ddef106c0b60b4
Cédric Kheirallah <cedric.kheirallah@student.uclouvain.be>
2022-11-25 10:28:20 +0100

    producteur-consommateur

--- producer_consumer.c
+++ producer_consumer.c
@@ -7,43 +7,77 @@
 #include <string.h>
 #include <semaphore.h>
 
+#define NProd 3
+#define NConso 3
+// à mettre dans le cmd
 
-#define N 10 // places dans le buffer => à mettre en entré commande
+#define N 8
 pthread_mutex_t mutex;
 sem_t empty;
 sem_t full;
 
-pthread_mutex_init(&mutex, NULL);
-sem_init(&empty, 0 , N);  // buffer vide
-sem_init(&full, 0 , 0);   // buffer vide
+int in_index = 0;
+int out_index = 0;
+int buffer[N];
 
 // Producteur
-void producer(void) {
+void* producer(void* pro) {
     int item;
-    while (true) {
-        item = produce(item);
-        sem_wait(&empty); // attente d'une place libre
+    for (int i = 0; i < 10; i++) {
+        item = rand(); // produce random int
+        sem_wait(&empty); // wait for free slots in_index buffer
         pthread_mutex_lock(&mutex);
         // section critique
-        insert_item();
+        buffer[in_index] = item;
+        printf("Producteur %d: inserted item %d at slot %d\n", *(int*) pro, buffer[in_index], in_index);
+        in_index = (in_index + 1) % N; // update index
         pthread_mutex_unlock(&mutex);
-        sem_post(&full); // il y a une place remplie en plus
+        sem_post(&full); // post that a slot has been filled
     }
 }
 
 // Consommateur
-void consumer(void) {
-    int item;
-    while (true) {
-        sem_wait(&full); // attente d'une place remplie
+void* consumer(void* con) {
+    for (int i = 0; i < 10; i++) {
+        sem_wait(&full); // wait for empty slots
         pthread_mutex_lock(&mutex);
         // section critique
-        item = remove(item);
+        int item = buffer[out_index];
+        printf("Consumer %d: removed item %d from slot %d\n", *(int*) con, item, out_index);
+        out_index = (out_index + 1) % N; // update index
         pthread_mutex_unlock(&mutex);
-        sem_post(&empty); // il y a une place libre en plus
+        sem_post(&empty); // post that a slot has been filled
     }
 }
 
 int main(int argc, char* argv[]) {
     printf("Hello there !\n");
+
+    pthread_t pro[8], con[8];
+
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&empty, 0, N);
+    sem_init(&full, 0, 0);
+
+    int a[8] = {1,2,3,4,5,6,7,8};
+
+    for (int i = 0; i < NProd; i++) {
+        pthread_create(&pro[i], NULL, producer, &a[i]);
+    }
+    for (int i = 0; i < NConso; i++) {
+        pthread_create(&con[i], NULL, consumer, &a[i]);
+    }
+
+    for (int i = 0; i < NProd; i++) {
+        pthread_join(pro[i], NULL);
+    }
+    for (int i = 0; i < NConso; i++) {
+        pthread_join(con[i], NULL);
+    }
+
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&empty);
+    sem_destroy(&full);
+
+    return 0;
 }
